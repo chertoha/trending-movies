6e1a936f132cd39b6a93f65d12c26cd6ab23aa3c
Anton <a.chertok@akatech.com.ua>
2023-01-02 22:42:55 +0200

    chakra install

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,20 @@
 import "../styles/globals.css";
 import Layout from "components/Layout";
+import theme from "utils/theme";
 import { Provider } from "react-redux";
 import { store } from "redux/store";
 
 import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <Provider store={store}>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
+      <ChakraProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
     </Provider>
   );
 };
